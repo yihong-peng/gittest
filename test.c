50946ba38e4a0f2bdbf13c240faa18474e5882d1
yihong-peng <34615165@qq.com>
2020-05-19 13:13:10 +0800

    branch test

---
+++ test.c
@@ -0,0 +1,14 @@
+
+int led = 13;    
+
+void setup() {                  // the setup() method is executed only once
+  pinMode(led, OUTPUT);         // the led PIN is declared as digital output
+}
+
+void loop() {                   // the loop() method is repeated
+  digitalWrite(led, HIGH);      // switching on the led 
+  delay(1000);                  // stopping the program for 1000 milliseconds
+  digitalWrite(led, LOW);       // switching off the led 
+  delay(1000);                  // stopping the program for 1000 milliseconds
+}
+
